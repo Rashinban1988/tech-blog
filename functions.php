c6e0fb355fab1b78da9b54a9d0b96136f0b5a850
kazuya yamamoto <83887766+mest-yamaru@users.noreply.github.com>
2024-10-27 23:02:19 +0900

    update functions.php

--- functions.php
+++ functions.php
@@ -54,8 +54,8 @@ function origin_theme_enqueue_scripts()
   wp_enqueue_script('jquery');
   wp_enqueue_script('add-script', get_stylesheet_directory_uri() . '/js/common.js', [], $timestamp, true);
 
-  $timestamp = date('Ymdgis', filemtime(get_stylesheet_directory() . '/scss/styles.css'));
-  wp_enqueue_style('add-common-style', get_stylesheet_directory_uri() . '/scss/styles.css', [], $timestamp);
+  $timestamp = date('Ymdgis', filemtime(get_stylesheet_directory() . '/styles/css/styles.css'));
+  wp_enqueue_style('add-common-style', get_stylesheet_directory_uri() . '/styles/css/styles.css', [], $timestamp);
 }
 add_action('wp_enqueue_scripts', 'origin_theme_enqueue_scripts');
 // Contact Form 7の自動pタグ無効化
